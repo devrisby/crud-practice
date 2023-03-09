b3843ec1f8545f593962034922ae656dde5e3ef9
WHOVION <eszrajs@gmail.com>
2023-03-09 15:27:04 -0800

    10-added input, able to submit

--- client/src/components/Movies.jsx
+++ client/src/components/Movies.jsx
@@ -1,10 +1,51 @@
+import React from 'react'
 import Movie from "./Movie"
+import { useState } from 'React'
 
 const Movies = ({movies}) => {
+
+    const [form, setForm] = useState({
+        name: '',
+        year: '',
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(form)
+    }
+
     return (
+    <div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                    <label htmlFor='name'>Movie:</label>
+                    <input 
+                        required
+                        type='text'
+                        id='name'
+                        placeholder='Enter Movie Name'
+                        value={form.name}
+                        onChange={ e => setForm({ ...form, name: e.target.value }) }
+                    />
+
+                    <label htmlFor='name'>Year:</label>
+                    <input 
+                        required
+                        type='number'
+                        id='year'
+                        placeholder='Year'
+                        value={form.year}
+                        onChange={ e => setForm({ ...form, year: e.target.value }) }
+                    />
+                <button type='submit'>Submit Movie</button>
+            </form>
+        </div>
+
         <div className="movies">
             {movies.map((m,i) => <Movie key={i} movie={m} />)}
         </div>
+
+    </div>
     )
 }
 
