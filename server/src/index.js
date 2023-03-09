989ea6e803d9866dd2c53474c4228229818fe7d2
devrisby <118779687+devrisby@users.noreply.github.com>
2023-03-09 01:23:58 -0500

    Setup start method for application

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,37 @@
 import express from "express";
 import cors from "cors";
+import * as config from './config.js'
+import { startDb } from "./data/db.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3001;
 
 app.get("/api/health", (req, res) => {
   res.json("This is working").status(200);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startHttp = (port) => {
+  const setupServer = (resolve, reject) => {
+    if(!port) reject("Port is missing");
+
+    const server = app.listen(port)
+    server.once("listening", () => resolve(`Server running on port ${port}`))
+    server.once("error", (err) => reject(`Server failed to start: ${err}`))
+  }
+
+  return new Promise(setupServer)
+}
+
+const main = async (port, mongoUri) => {
+  try {
+    const res = await Promise.all([startHttp(port), startDb(mongoUri)])
+    res.forEach(msg => console.log(msg))
+  } catch (err) {
+    console.error("Failed to start application")
+    console.error(err)
+  }
+}
+
+main(config.PORT, config.MONGO_URI);
