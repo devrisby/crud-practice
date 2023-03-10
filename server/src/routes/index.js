920902fbbb27f9f823f96f2fbc40e72ad0e87f15
Robert Snow <robjsnow@gmail.com>
2023-03-10 14:59:54 -0600

    changed post to get for getMovie

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,7 +5,7 @@ import movieController from "../controllers/movieController.js";
 
 router.get('/api/movies', movieController.getAllMovies);
 router.post('/api/movies', movieController.addMovie);
-router.post('/api/movies/:id', movieController.getMovie);
+router.get('/api/movies/:id', movieController.getMovie);
 router.put('/api/movies/:id', movieController.updateMovie);
 router.delete('/api/movies/:id',movieController.deleteMovie);
 
